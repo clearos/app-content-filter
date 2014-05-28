f764dc25f23d931a7d59f4b6d8090364fa068cb5
pcbaldwin <pbaldwin@clearfoundation.com>
2014-05-28 12:14:36 -0400

    Cleaned up network configuration event to prep for LAN IP auto tune

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'content_filter';
-$app['version'] = '1.5.5';
+$app['version'] = '1.6.2';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-content-filter.spec
+++ packaging/app-content-filter.spec
@@ -1,7 +1,7 @@
 
 Name: app-content-filter
 Epoch: 1
-Version: 1.5.5
+Version: 1.6.2
 Release: 1%{dist}
 Summary: Content Filter
 License: GPLv3
