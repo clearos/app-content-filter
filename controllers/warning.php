ef44ebeec78b7ccdbe43effdbb30bf694db8c530
pcbaldwin <pbaldwin@clearfoundation.com>
2014-06-07 01:30:13 -0400

    Cleaned out deprecated split() function calls [tracker #1700]

--- controllers/warning.php
+++ controllers/warning.php
@@ -79,9 +79,8 @@ class Warning extends ClearOS_Controller
         $reason = base64_decode(strtr($reason, '-_.', '+/='));
 
         if (preg_match('/:.*\(.*\)/', $reason)) {
-            // FIXME: split() is deprecated
-            list($reason_header, $details) = split('\(', $reason, 2);
-            list($description, $summary) = split(':', $reason_header, 2);
+            list($reason_header, $details) = preg_split('/\(/', $reason, 2);
+            list($description, $summary) = preg_split('/:/', $reason_header, 2);
 
             // Full report details
             $details = trim($details);
