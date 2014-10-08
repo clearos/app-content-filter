5a98be4745af7dfd291fbb034ceb82fbd4c8abf9
pcbaldwin <pbaldwin@clearfoundation.com>
2014-10-08 15:04:38 -0400

    Prepped for test build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'content_filter';
-$app['version'] = '1.6.5';
+$app['version'] = '2.0.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
